f6b12c658e71c9d8be3f9edd8020a55fbc7f97e8
varcal-dev <cesarlea2010@hotmail.com>
2025-05-09 04:25:38 -0500

    Bycrip y hash

--- src/main/java/com/varcal/cheermanager/Service/AuthService.java
+++ src/main/java/com/varcal/cheermanager/Service/AuthService.java
@@ -1,8 +1,10 @@
 package com.varcal.cheermanager.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
+import com.varcal.cheermanager.Models.Auth.Usuario;
 import com.varcal.cheermanager.repository.UserRepository;
 
 @Service
@@ -13,7 +15,15 @@ public class AuthService {
 
     public boolean authenticate(String email, String password) {
         return userRepository.findByEmail(email)
-                .map(user -> user.getPasswordHash().equals(password)) // Comparar contraseñas
+                .map((Usuario user) -> BCrypt.checkpw(password, user.getPasswordHash())) // Comparar contraseñas
                 .orElse(false); // Si no se encuentra el usuario, devolver false
+    }
+
+    public void registerUser(String email, String password) {
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+        Usuario user = new Usuario();
+        user.setEmail(email);
+        user.setPasswordHash(hashedPassword);
+        userRepository.save(user);
     }
 }
--- src/main/java/com/varcal/cheermanager/config/WebConfig.java
+++ src/main/java/com/varcal/cheermanager/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.varcal.cheermanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,7 +13,7 @@ public class WebConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/api/**") // Cambiado para incluir el endpoint de autenticación
                         .allowedOrigins("http://localhost:3000") // URL del frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
